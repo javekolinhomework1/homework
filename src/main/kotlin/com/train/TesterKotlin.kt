13bcaf2c7dcf6e29be7a3a0878b548323773a54d
javakolinhomework1 <angel524188@gmail.com>
2019-09-27 21:26:48 +0800

    Java and kotlin variable,data type

--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -1,16 +1,30 @@
 package com.train
 
+import java.util.*
+
 fun main() {
-   // println("Please enter number of tickets:")
-    //Tester2().Tester()
-    var t = Tester2()
-    t.Tester()
+
+    val scan = Scanner(System.`in`)
+    println("Please enter number of tickets:")
+    var numberticket = scan.nextInt()
+    println("Please enter number of tickets:")
+    var roundtriptickets = scan.nextInt()
+
+    val t = Tester2(numberticket, roundtriptickets)
+    t.print()
 
 
 }
 
-class Tester2{
-    fun Tester(){
-        println("Please enter number of tickets:")
+class Tester2(var numberticket:Int, var roundtriptickets:Int){
+    var onewayprice = 1000;
+    var rountripdis = 0.9f;
+    var roundtripprice = 2000*rountripdis;
+    fun print(){
+        println("Total tickets: $numberticket \tRound-trip: $roundtriptickets \tTotal: ${getPrice()}")
+    }
+
+    fun getPrice(): Float{
+        return ((numberticket-roundtriptickets)*onewayprice + roundtriptickets*roundtripprice);
     }
 }
